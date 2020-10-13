3a94110bfa31a838483041911b239340fe622ba5
mozumderTushar <itztushar55@gmail.com>
2020-10-13 16:38:50 +0600

    fake data load

--- index.js
+++ index.js
@@ -17,16 +17,33 @@ const port = 5000
 
 
 
-const client = new MongoClient(uri, { useNewUrlParser: true,  useUnifiedTopology: true });
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 client.connect(err => {
-  const collection = client.db("creativeAgency").collection("services");
-  console.log('db connected');
- 
+    const servicesCollection = client.db("creativeAgency").collection("services");
+    const feedbackCollection = client.db("creativeAgency").collection("feedback");
+    console.log('db connected');
+
+    //services
+    app.get('/services', (req, res) => {
+        servicesCollection.find({})
+            .toArray((err, documents) => {
+                res.send(documents)
+            })
+    })
+
+    //client feedback
+    app.get('/feedback', (req, res) => {
+        feedbackCollection.find({})
+            .toArray((err, documents) => {
+                res.send(documents)
+            })
+    })
+
 });
 
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+    res.send('Hello World!')
 })
 
 app.listen(process.env.PORT || port)
